962d26baedc941ffb7aba84331e3560503f995d3
Adrien Brizard <brizard.adrien@gmail.com>
2016-03-24 23:20:28 +0100

    Setting up webpack and other stuff.

---
+++ js/components/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by krelix on 24/03/2016.
+ */
+
+import React from 'react'
+
+export default class Main extends React.Component {
+  render(){
+    return (
+      <div>
+        <h2>HELLO WORLD</h2>
+        <p>THIS IS SOME FUN TEST.</p>
+      </div>
+    )
+  }
+}
---
+++ js/index.js
@@ -0,0 +1,13 @@
+/**
+ * Created by krelix on 24/03/2016.
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Main from './components/main';
+
+
+ReactDOM.render(
+  <Main />,
+  document.getElementById('mainContainer')
+);
