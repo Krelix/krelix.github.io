4145c9113c765a87165316a6a394c3ef04d669f0
Adrien Brizard <brizard.adrien@gmail.com>
2016-04-15 14:33:31 +0200

    Separation between node_modules library and actual app

--- js/components/content/about.js
+++ js/components/content/about.js
@@ -8,7 +8,7 @@ export default function render() {
     <div>
       <h1>About</h1>
       <br />
-      <p>Hello, my name is Adrien. I usually go by the "Krelix" handle on many places.</p>
+      <p>Hello, my name is Adrien. I usually go by the "Krelix" handle on many places. I've been a developer for only 5 years.</p>
     </div>
   );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-  entry: './js/index.js',
+  entry: {
+    app: './js/index.js',
+    vendor: ['react', 'react-dom', 'react-router']
+  },
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'app.js'
@@ -10,10 +14,21 @@ module.exports = {
     loaders: [
       {
         test: /\.js?$/,
-        exclude: /(build|node_modules|bower_components)/,
+        exclude: /(build|node_modules)/,
         loader: 'babel'
       }
     ]
-  }
-}
-;
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      minChunks: 3,
+      name: "vendor",
+      filename : "vendor.bundle.js"
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings : false,
+      }
+    })
+  ]
+};
