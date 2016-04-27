16ca4fbfc9bc4243005f4e5f810e257ed50d150f
Adrien Brizard <brizard.adrien@gmail.com>
2016-04-27 21:08:40 +0200

    Picture resizing (and src change)

--- js/components/content/project-list.js
+++ js/components/content/project-list.js
@@ -10,7 +10,7 @@ export default class ProjectList extends React.Component {
     return (
       <div>
         <h1>Projects</h1>
-        <ProjectItem name="My little project" imagePath="assets/images/project_pic.png">
+        <ProjectItem name="My little project" imagePath="assets/images/project_pic_120x120.png">
           This is some description for the project. It's not complete as it is nothing but a placeholder but I think it's better
           than a random Lorem Ipsum, since it's actual text I've written and i is far more likely to represent the kind of bullshit
           I'll write for real.
