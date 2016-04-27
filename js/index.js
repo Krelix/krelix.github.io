4578bfd8349af7111051819008bd5f7013799e09
Adrien Brizard <brizard.adrien@gmail.com>
2016-04-27 07:40:12 +0200

    Setting up of a dev version with hot reloading (not particularly important, but it's good practice)

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Routes from './components/routes';
+import css from '../assets/stylus/main.styl';
 
 ReactDOM.render(
   <Routes />,
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 var path = require('path');
 var webpack = require('webpack');
+var _ = require('lodash');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
+var GENERIC_PARAMS = {
   entry: {
     app: './js/index.js',
-    vendor: ['react', 'react-dom', 'react-router']
   },
   output: {
-    path: path.join(__dirname, 'build'),
     filename: 'app.js'
+  }
+};
+
+var DEV_PARAMS = {
+  output: {
+    path: path.join(__dirname, 'dev'),
   },
   module: {
     loaders: [
@@ -16,19 +23,49 @@ module.exports = {
         test: /\.js?$/,
         exclude: /(build|node_modules)/,
         loader: 'babel'
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!stylus-loader'),
+        exclude: /node_modules/
       }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "DEV VERSION - Home Page",
+      filename: 'index.html',
+      cache: true,
+      template: 'assets/dev_index.html'
+    }),
+    new ExtractTextPlugin("main.css")
+  ]
+};
+
+var DIST_PARAMS = {
+  entry: {
+    vendor: ['react', 'react-dom', 'react-router']
+  },
+  output : {
+    path: path.join(__dirname, 'build'),
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
       minChunks: 3,
       name: "vendor",
-      filename : "vendor.bundle.js"
+      filename: "vendor.bundle.js"
     }),
     new webpack.optimize.UglifyJsPlugin({
       compress: {
-        warnings : false,
+        warnings: false,
       }
     })
   ]
-};
+}
+
+
+
+module.exports = (function(isProduction){
+  var merged =  _.merge(GENERIC_PARAMS, isProduction ? DIST_PARAMS : DEV_PARAMS);
+  return merged;
+})(false);
